f8ee6ddaa517c8ae2e4c51c7653dc1cb3d9fd26a
Jon Brammer <jbrammer@tableau.com>
2019-08-01 09:24:33 -0700

    Clean up styles

--- App.js
+++ App.js
@@ -6,36 +6,29 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Button, Platform, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import { useScreens } from 'react-native-screens';
-import { bold } from 'ansi-colors';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 useScreens();
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-const buttonPressed = () => {};
 
 class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, flexDirection: 'column' }}>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.verticalContainer}>
+        <View style={styles.centeredContainer}>
           <Text>Hi I'm the FIRST screen</Text>
         </View>
-        <TouchableOpacity style={{ flex: 1 }} onPress={() => this.props.navigation.push('Second')}>
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#3A8EED', borderRadius: 20, margin: 20 }}>
-            <Text style={{ color: 'white', fontWeight: 'bold' }}>Tap me for second screen</Text>
-          </View>
-        </TouchableOpacity>
+        <View style={styles.flexOne}>
+          <TouchableOpacity
+            style={[styles.centeredContainer, styles.buttonExtras]}
+            onPress={() => this.props.navigation.push('Second')}
+          >
+            <Text style={styles.buttonText}>Tap me for second screen</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -44,11 +37,11 @@ class HomeScreen extends React.Component {
 class SecondScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, flexDirection: 'column' }}>
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={styles.verticalContainer}>
+        <View style={styles.centeredContainer}>
           <Text>Hi I'm the SECOND screen</Text>
         </View>
-        <View style={{flex: 1}}/>
+        <View style={styles.flexOne}/>
       </View>
     )
   }
@@ -63,20 +56,25 @@ const AppNavigator = createStackNavigator({
 export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
-  container: {
+  verticalContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  centeredContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  flexOne: {
+    flex: 1,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  buttonExtras: {
+    backgroundColor: '#3A8EED',
+    borderRadius: 20,
+    margin: 20
   },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
 });
